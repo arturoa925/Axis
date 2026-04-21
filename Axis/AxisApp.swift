44bf29d06ba5c64297d3418566aeb97e35965951
arturoa925 <ayalaarturo925@gmail.com>
2026-04-21 07:56:28 -0700

    Initial Commit

---
+++ Axis/AxisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxisApp.swift
+//  Axis
+//
+//  Created by Arturo Ayala on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct AxisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AxisTests/AxisTests.swift
@@ -0,0 +1,19 @@
+//
+//  AxisTests.swift
+//  AxisTests
+//
+//  Created by Arturo Ayala on 4/21/26.
+//
+
+import Testing
+@testable import Axis
+
+struct AxisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
